436214d96435518e04e6e5ff6997b6a782390bc2
iNAzT <NAzT@inazts-macbook.local>
2010-04-01 17:53:49 +0700

    add list of allowed username

--- grails-app/controllers/com/Ads/UserController.groovy
+++ grails-app/controllers/com/Ads/UserController.groovy
@@ -3,20 +3,33 @@ import grails.converters.*
 class UserController {
 		def apiUrl = "http://twitter.com/statuses/user_timeline.json"
 	    def oauthService
- 
+ 		def allowedUser=["NAzT", 'traffy','wapst7'].collect { it.toLowerCase()  }
     def index = {  render 'index' }
 	def login = {   }
 	def permission = {  
-	 	println 'session'+ session
+	 	
         if (session.oauthToken == null) {
 		 	println "error no session!"
             redirect(uri:"/")
 			return ;
         }
-		
 	        def response = oauthService.accessResource(
 	                apiUrl, 'twitter', [key:session.oauthToken.key, secret:session.oauthToken.secret])
 	        def statuses = JSON.parse(response)
-			render 'Hello ' +  statuses.user.screen_name.get(0)
+			
+			def authenticatedUser=statuses.user.screen_name.get(0)
+			
+			if(  authenticatedUser.toLowerCase() in allowedUser)
+			{
+				session["user"]  = authenticatedUser;
+/*				render 'Hello ' +  authenticatedUser
+				render " Allowed."*/
+			  redirect(controller:'data',action:'list')
+			}
+			else
+			{
+				render 'Hello ' +  authenticatedUser
+				render " Don't Allowed."
+			}
 		 }
 }
