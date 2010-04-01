fdc2aa1046419d9249f21ba31708ca7b0fc57006
iNAzT <NAzT@inazts-macbook.local>
2010-04-01 17:07:59 +0700

    add Security Filter

---
+++ grails-app/conf/com/Ads/SecurityFilters.groovy
@@ -0,0 +1,32 @@
+/*package com.Ads
+
+class SecurityFilters {
+
+    def filters = {
+       loginCheck(controller:'*', action:'*') {
+           before = {
+   			  if (params.controller == null) 
+				{ 
+	                 redirect(controller:'user',action:'login')*/
+	                 /* When using Tomcat and the controller is null you must 
+	                    return true. Otherwise an exception is thrown. */
+/*	                 return true
+			    }
+				else if (controllerName.equals('oauth'))
+				{
+					println 'dd'
+					return true
+				}				
+	          else if(!session.user && !actionName.equals('login')) {
+                  redirect(controller:'user',action:'login')
+                  return false
+               }
+
+			
+	 	
+          }
+
+       }       
+    }
+    
+}*/
---
+++ test/unit/com/Ads/SecurityFiltersTests.groovy
@@ -0,0 +1,17 @@
+package com.Ads
+
+import grails.test.*
+
+class SecurityFiltersTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
