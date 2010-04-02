796b74997aba438fd15bbc7ccb1bbef9a0d47ea8
iNAzT <NAzT@inazts-macbook.local>
2010-04-02 12:07:46 +0700

    change default value of Data frequency

--- grails-app/domain/com/Ads/Data.groovy
+++ grails-app/domain/com/Ads/Data.groovy
@@ -3,7 +3,7 @@ package com.Ads
 class Data {
 	static belongsTo = [ adsType : Type ]  
 	String message
-	Integer frequency=0
+	Integer frequency=1
 	
     static constraints = {
 		message(unique:true,nullable:false,blank:false)
