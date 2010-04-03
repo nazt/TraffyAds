82461eb173e06b3ef408433a1245e053a4d4aa5a
iNAzT <NAzT@inazts-macbook.local>
2010-04-03 09:04:52 +0700

    add more init data in bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,8 +2,12 @@
 class BootStrap {
 
      def init = { servletContext ->
-		new com.Ads.Type(name:"Information").save()
-		new com.Ads.Type(name:"Relax").save()		
+		def info=new com.Ads.Type(name:"Information").save()
+		def relax=new com.Ads.Type(name:"Relax").save()	
+		def testInfo=new com.Ads.Data(message:"ทดสอบInfo",adsType:info).save()
+		new com.Ads.Data(message:"ทดสอบRelax",adsType:relax,frequency:4).save()		
+		
+		new com.Ads.Stat(adsMessage:testInfo).save()
      }
      def destroy = {
 	
