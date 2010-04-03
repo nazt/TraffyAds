47310c8eca7c9d783fcb02eeed2355e4b73ae673
iNAzT <NAzT@inazts-macbook.local>
2010-04-03 09:10:47 +0700

    add prefix for table name

--- grails-app/domain/com/Ads/Data.groovy
+++ grails-app/domain/com/Ads/Data.groovy
@@ -4,9 +4,16 @@ class Data {
 	static belongsTo = [ adsType : Type ]  
 	String message
 	Integer frequency=1
-	
+	static mapping = {
+      table 'twitter_ads_data'     
+    }	
     static constraints = {
 		message(unique:true,nullable:false,blank:false)
 		frequency()
     }
+	String toString()
+	{
+		/*"${id} - ${message} (${frequency})"*/
+		"${message}"
+	}
 }
--- grails-app/domain/com/Ads/Type.groovy
+++ grails-app/domain/com/Ads/Type.groovy
@@ -3,7 +3,9 @@ package com.Ads
 class Type {
 	static hasMany = [adsMessage:Data]	
 	String name
-	
+	static mapping = {
+      table 'twitter_ads_type'     
+    }
     static constraints = {
 		name(unique:true,nullable:false,blank:false)
     }
